a44d547eb0e7241d959210f73301d91bf4300b4d
huutrong2307 <74829070+huutrong2307@users.noreply.github.com>
2022-12-11 11:38:12 +0700

    Add put and delete user profile API

--- app/hello_world/views.py
+++ app/hello_world/views.py
@@ -1,62 +1,67 @@
 from django.shortcuts import render
+from django.contrib.auth import logout 
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseNotFound
 from hello_world.models import Profile, User
-from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import ProfileSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework import status
 
 def hello_world(request):
     return HttpResponse("<h1>Hello world</h1>")
 
-# @csrf_exempt 
-# @api_view(('GET',))
-# def profile(request, user_id):
-#     try: 
-#         cur_user = User.objects.get(id=user_id)
-#         cur_user_profile = Profile.objects.get_or_create(user=cur_user)
-#         if request.method == 'GET':
-#             # template = loader.get_template('get_user_profile.html')
-#             context = {
-#                 'username': cur_user.username,
-#                 'gender': cur_user.profile.gender,
-#                 'about_me': cur_user.profile.about_me
-#             }
-#             print(cur_user_profile.get_username())
-            
-#             serialize = ProfileSerializer(cur_user_profile)
-#             return Response(serialize.data)
-#             # return render(request,'get_user_profile.html',context)
-#         elif request.method == 'PUT':
-#             if(not cur_user.is_anonymous()):
-#                 cur_user_profile = cur_user.profile 
-#                 cur_user.username = request.POST.get('username')
-#                 cur_user_profile.gender = request.POST.get('gender')
-#                 cur_user_profile.about_me = request.POST.get('about_me')
-#                 cur_user_profile.save()
-#                 cur_user.save()
-#                 return HttpResponse("<h1>Successfully updated</h1>")
-#         elif request.method == 'DELETE':
-#             return
 
-#     except User.DoesNotExist:
-#         return HttpResponseNotFound(f"User With ID:{user_id} Does Not Exist!")
 
     
 class ProfileAPI(APIView):
     def get(self, request, user_id,format = None):
         try: 
             cur_user = User.objects.get(id=user_id)
-            print(cur_user.username)
+           
             Profile.objects.get_or_create(user=cur_user)
             cur_user_profile = cur_user.profile
-            print(cur_user_profile.phone)
+            
             serializer = ProfileSerializer(cur_user_profile)
             return Response(serializer.data)
         except User.DoesNotExist:
             return HttpResponseNotFound(f"User With ID:{user_id} Does Not Exist!")
 
+    def put(self, request, user_id):
+        try: 
+            cur_user = User.objects.get(id=user_id)
+            Profile.objects.get_or_create(user=cur_user)
+            cur_user_profile = cur_user.profile
+
+            
+            cur_user.username = request.POST.get('username')
+            cur_user_profile.phone = request.POST.get('phone')
+            cur_user_profile.gender = request.POST.get('gender')
+            cur_user_profile.is_premium = request.POST.get('is_premium')
+            cur_user_profile.about_me = request.POST.get('about_me')
+            cur_user_profile.birthday = request.POST.get('birthday')
+            cur_user_profile.address = request.POST.get('address')
+            cur_user_profile.big_picture_url = request.POST.get('big_picture_url')
+            cur_user_profile.small_picture_url = request.POST.get('small_picture_url')
+            cur_user_profile.save()
+            cur_user.save()
+
+
+            serializer = ProfileSerializer(cur_user_profile)
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            return HttpResponseNotFound(f"User With ID:{user_id} Does Not Exist!")
+
+    def delete(self,request, user_id):
+        try: 
+            cur_user = User.objects.get(id=user_id)
+            Profile.objects.get_or_create(user=cur_user)
+            cur_user_profile = cur_user.profile
+            logout(request)
+            
+            cur_user.is_active=False
+            cur_user_profile.is_active=False
+            return Response(status=status.HTTP_204_NO_CONTENT) 
+        except User.DoesNotExist:
+            return HttpResponseNotFound(f"User With ID:{user_id} Does Not Exist!")
